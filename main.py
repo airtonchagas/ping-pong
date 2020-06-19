f3401a2c00986335cd14fb07837c2387cbb56ebc
Airton Chagas <airton_0208@live.com>
2020-06-18 22:01:55 -0300

    subindo audio

--- main.py
+++ main.py
@@ -12,34 +12,32 @@ from widgets         import Pong, Raquete, Bola
 from telas           import *
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-# Carrega nosso arquivo de configurações
 Config.read("config.ini")
 Config.set('graphics', 'window_state', 'maximized')
 
-# Cria nosso Gerenciador de Telas
 screen_manager = ScreenManager()
 
 class PongApp(App):
 
     def build(self):
 
-        # Carrega o áudio
+
         sound = SoundLoader.load('audio/bg-music.mp3')
 
-        # Verifica se houve o carregamento do nosso áudio e coloca para tocar
+
         if sound:
             sound.play()
 
-        # Objeto do nosso jogo
+
         pong = Pong(screen_manager=screen_manager)
 
-        # Cria a Tela de Jogo
+
         tela_jogo = TelaJogo(name="jogo")
 
-        # Adiciona o Widget Pong
+
         tela_jogo.add_widget(pong)
 
-        # Adiciona as telas ao nosso gerenciador
+
         screen_manager.add_widget(TelaMenu(name='menu'))
         screen_manager.add_widget(tela_jogo)
         screen_manager.add_widget(TelaVencedor1(name='vencedor_1'))
--- widgets/Pong.py
+++ widgets/Pong.py
@@ -6,16 +6,10 @@ from kivy.core.window import Window
 
 # Definição do
 class Pong(Widget):
-    """
-    Esse elemento contém todos os elementos do jogo (campo, raquetes e
-    bolinha). Nele também está a lógica de colisão da bolinha com as
-    paredes da janela à fim de atualizar o placar do jogo.
-    """
 
-    # Referencia o objeto Bola definido no nosso arquivo .kv
     bola = ObjectProperty(None)
 
-    # Referencia os objetos Raquete definidos no nosso arquivo .kv
+
     raquete_1 = ObjectProperty(None)
     raquete_2 = ObjectProperty(None)
 
@@ -29,34 +23,33 @@ class Pong(Widget):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
 
-    # Põe a bola em jogo
+
     def servico(self, vel=(4, 0)):
 
-        # Posiciona a bola no centro da tela
+
         self.bola.center = self.center
 
-        # Seta a velocidade da bola
+
         self.bola.velocidade = vel
 
-    # Atualiza nosso jogo
+
     def atualiza(self, dt):
 
 
-        # Faz a bola se mover
+
         self.bola.movimenta()
 
-        # Rebate a bola caso haja colisão com a bolinha
+
         self.raquete_1.rebate_bola(self.bola)
         self.raquete_2.rebate_bola(self.bola)
 
-        # Verifica se a bola atingiu o topo da janela
+
         if (self.bola.y < 0) or (self.bola.top > self.height):
             self.bola.velocidade_y *= -1
 
-        # Verifica se colidiu com o lado esquerdo da janela para atualizar o
-        # placar do jogo
+
         if self.bola.x < self.x:
-            # +1 para o placar da raquete_2
+
             self.raquete_2.placar += 1
 
             if self.raquete_2.placar >= 3:
@@ -67,13 +60,11 @@ class Pong(Widget):
 
                 return
 
-            # Reinicia o jogo com a bola saindo pelo lado esquerdo
+
             self.servico(vel=(4, 0))
 
-        # Verifica se colidiu com o lado direito da janela para atualizar o
-        # placar do jogo
         if self.bola.x > self.width:
-            # +1 para o placar da raquete_1
+
             self.raquete_1.placar += 1
 
             if self.raquete_1.placar >= 3:
@@ -84,7 +75,7 @@ class Pong(Widget):
 
                 return
 
-            # Reinicia o jogo com a bola saindo pelo lado direito
+
             self.servico(vel=(-4, 0))
 
     # # Captura o evento on_touch_move (arrastar de dedo na tela)
@@ -122,19 +113,19 @@ class Pong(Widget):
 
     def remove_btn(self, btn):
 
-        # Remove o botão de iniciar jogo
+
         self.remove_widget(btn)
 
     def comeca_jogo(self):
 
-        # Pôe a bola em jogo
+
         self.servico()
 
-        # Agendamento da função "atualiza" a cada 1/120 = 0,008s
+
         Clock.schedule_interval(self.atualiza, 1.0/120.0)
 
     def reinicia_jogo(self):
-        # Pôe a bola em jogo
+
         self.servico(vel=(4,0))
 
         self.raquete_1.placar = 0
